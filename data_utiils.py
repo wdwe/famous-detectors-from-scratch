6856c9884ae9f9bec55c48af5edec52e4e4b7ead
wdwe <43853049+wdwe@users.noreply.github.com>
2021-02-20 08:53:20 -0500

    Add normalization to tensor and rename/debug
    All _trans renamed into _affine_trans.
    Fixed torch.device string in MetaImages' cuda() method.

--- data_utiils.py
+++ data_utiils.py
@@ -12,31 +12,50 @@ class MetaImages:
         targets: a list of None or dict where targets[i]["bbox"] should be a Tensor of shape [batchsize, 4] in (xmin, ymin, xmax, ymax) format
 
     """
-    def __init__(self, images, targets = None, size = (640, 640)):
+    def __init__(
+            self, 
+            images, 
+            targets = None, 
+            size = (640, 640),
+            normalize = True,
+            mean = (0.485, 0.456, 0.406), 
+            std = (0.229, 0.224, 0.225)
+        ):
+
+        # note: for training, normalisation may be better done in dataset
+        # so that the process can be parallelised
+        if normalize:
+            for image in images:
+                if image.max() > 1:
+                    image.div_(255)
+            images = [F.normalize(image, mean, std) for image in images]
+            
+
         self.size = size
         self.orig_sizes = []
         for img in images:
             self.orig_sizes.append(img.shape)
         if targets is None:
             targets = [None] * len(images)
-        self._transform(images, targets)
+        self._affine_trans(images, targets)
         if not self.targets:
             self.targets = None
 
-    def _transform(self, images, targets):
+
+    def _affine_trans(self, images, targets):
         self.images = []
         self.scales = []
         self.targets = []
         for i, image in enumerate(images):
-            image, scale = self._trans_img(image)
+            image, scale = self._affine_trans_img(image)
             self.scales.append(scale)
             self.images.append(image)
             if targets is not None:
-                target = self._trans_target(targets[i], scale)
+                target = self._affine_trans_target(targets[i], scale)
                 self.targets.append(target)
         self.images = torch.stack(self.images)
 
-    def _trans_img(self, image):
+    def _affine_trans_img(self, image):
         _, h, w = image.shape
         # compute size
         H, W = self.size
@@ -50,7 +69,7 @@ class MetaImages:
 
         return image, scale
 
-    def _trans_target(self, target, scale):
+    def _affine_trans_target(self, target, scale):
         target["bbox"] *= scale
         return target
 
@@ -62,5 +81,5 @@ class MetaImages:
             ]
 
     def cuda(self, num = 0):
-        device = torch.device(f"cuda: {num}")
+        device = torch.device(f"cuda:{num}")
         self.to(device)
