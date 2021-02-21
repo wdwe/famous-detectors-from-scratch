1403654504b974907efef71b854e69de1d7026ac
wdwe <43853049+wdwe@users.noreply.github.com>
2021-02-20 20:59:18 -0500

    Create models/ and moved faster_rcnn/ to it.

--- models/faster_rcnn/faster_rcnn.py
+++ models/faster_rcnn/faster_rcnn.py
@@ -11,7 +11,15 @@ class FasterRCNN_res50(nn.Module):
             [r.conv1, r.bn1, r.relu, r.maxpool, r.layer1, r.layer2, r.layer3, r.layer4]
         )
 
+    def extract_features(self, images):
+        for m in self.backbone:
+            images = m(images)
+        return images
+
     def forward(self, meta_img):
         images = meta_img.images
         target = meta_img.targets
+        features = self.extract_features(images)
+        
+        return features
 
