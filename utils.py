ed5bf248109d51130f4b7b0774f713bd091958ec
wdwe <43853049+wdwe@users.noreply.github.com>
2021-02-25 21:17:27 -0500

    Removed collate_fn.

--- utils.py
+++
@@ -1,5 +0,0 @@
-def collate_fn(batch):
-    # split batch from [(img_1, target_1), ..., (img_n, target_n)]]
-    # to
-    # ((img_1, ..., img_n), (target_1, ..., target_n))
-    return tuple(zip(*batch))
