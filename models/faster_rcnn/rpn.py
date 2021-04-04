ffd5a73e9f3b5c4b05fb2192b332b1b6a8f8ec3a
wdwe <43853049+wdwe@users.noreply.github.com>
2021-04-04 17:15:57 +0800

    Finished RPN

--- models/faster_rcnn/rpn.py
+++ models/faster_rcnn/rpn.py
@@ -2,16 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.ops as ops
+from utils import BoxCoder
 
 class RPN(nn.Module):
     def __init__(
         self,
         rpn_head,
         anchor,
+        bbox_coder = None,
         train_fg_iou = 0.7,
         train_bg_iou = 0.3,
         train_pos_samples = 128,
-        train_neg_samples = 128
+        train_neg_samples = 128,
+        num_output_bboxes = 2000
     ):
         super().__init__()
         self.rpn_head = rpn_head
@@ -20,7 +23,9 @@ class RPN(nn.Module):
         self.train_bg_iou = train_bg_iou
         self.train_pos_samples = train_pos_samples
         self.train_neg_samples = train_neg_samples
-        
+        if bbox_coder is None:
+            self.bbox_coder = BoxCoder()
+        self.num_output_bboxes = num_output_bboxes
 
 
     def _compute_loss(self, objness, bbox_regress, anchors, labels, matched_bboxes):
@@ -28,7 +33,6 @@ class RPN(nn.Module):
         batch_size = len(objness)
         offset_pos_idc = []
         offset_train_idc = []
-        
         for i in range(batch_size):
             labels_per_img = labels[i]
             all_pos_idc = torch.flatten((labels_per_img == 1).nonzero())
@@ -62,14 +66,25 @@ class RPN(nn.Module):
 
         # compute loss now
         objness_preds = objness[offset_train_idc]
-        objness_labels = labels[offset_train_idc]
+        objness_labels = labels[offset_train_idc].float()
         objness_loss = F.binary_cross_entropy_with_logits(objness_preds, objness_labels)
 
+        gt_bboxes = matched_bboxes[offset_pos_idc]
+        pred_bboxes = bbox_regress[offset_pos_idc]
+        gt_bboxes = ops.box_convert(gt_bboxes, "xyxy", "cxcywh")
+        anchors = anchors[offset_pos_idc]
+        gt_encoding = self.bbox_coder.encode(anchors, gt_bboxes)
+        # need to multiply by four as we are taking the average, across the batch,
+        # of the sum of smooth l1 loss over the 4 bbox parameters
+        bbox_loss = F.smooth_l1_loss(pred_bboxes, gt_encoding, beta = 1.0) * 4
 
-            
+        losses = {
+            "rpn_logit": objness_loss,
+            "rpn_bbox": bbox_loss
+        }
 
-
-
+        return losses
+        
 
 
     def assign_anchors_to_targets(self, anchors, targets):
@@ -89,7 +104,6 @@ class RPN(nn.Module):
         for anchors_per_img, targets_per_img in zip(anchors, targets):
             device = anchors_per_img.device
             target_bboxes = targets_per_img["bbox"].to(device)
-            print(anchors_per_img.shape)
             if len(target_bboxes) == 0:
                 labels_per_img = torch.zeros((len(anchors_per_img), ), dtype = torch.long, device = device)
                 matched_bboxes_per_img = torch.zeros_like(anchors_per_img)
@@ -130,7 +144,7 @@ class RPN(nn.Module):
         """
         set label to -1 if its corresponding anchors cross the image boundary
         """
-        labels = []
+        filtered_labels = []
         H, W = image_size
         for i, labels_per_image in enumerate(labels):
             anchors_per_img = anchors[i]
@@ -139,8 +153,22 @@ class RPN(nn.Module):
                  (anchors_per_img[:, 2] > W) + (anchors_per_img[:, 3] > H)) > 0
             ignore_idc = torch.nonzero(ignore_bool, as_tuple=True)[0]
             labels_per_image[ignore_idc] = -1
-            labels.append(labels_per_image)
+            filtered_labels.append(labels_per_image)
+        return filtered_labels
 
+
+
+    def filter_output_bboxes(self, objness, bbox_preds, image_size):
+        # bbox_preds in xc, yc, w, h
+        batch_size = len(objness)
+        bboxes = []
+        for i in range(batch_size):
+            bboxes_preds_per_img = bbox_preds[i]
+            bboxes_preds_per_img = ops.box_convert(bboxes_preds_per_img, "cxcywh", "xyxy")
+            bboxes_preds_per_img = ops.clip_boxes_to_image(bboxes_preds_per_img, image_size)
+            nms_bboxes = ops.nms(bboxes_preds_per_img, objness[i].flatten(), 0.7)
+            bboxes.append(nms_bboxes[:self.num_output_bboxes])
+        return bboxes
 
 
     def forward(self, features, targets = None, image_size = (640, 640)):
@@ -153,21 +181,26 @@ class RPN(nn.Module):
         objness = objness.view((objness.shape[0], -1, 1))
         # bbox_regress is of the shape [batchsize, 4 * num_anchors, feat_h, feat_w]
         shape = bbox_regress.shape
-        bbox_regress = bbox_regress.view((shape[0], shape[1]/4, 4, shape[2], shape[3]))
-        bbox_regress = bbox_regress.permute(0, 2, 3, 1)
-        bbox_regress = bbox_regress.view((shape[0], -1, 4))
+        bbox_regress = bbox_regress.view((shape[0], int(shape[1]/4), 4, shape[2], shape[3]))
+        bbox_regress = bbox_regress.permute(0, 1, 3, 4, 2)
+        bbox_regress = bbox_regress.reshape((shape[0], -1, 4))
         # anchors is in the format of [batchsize, k, feat_h, feat_w, 4]
         anchors = self.anchor(features)
         # flatten the anchors
         anchors = anchors.view((anchors.shape[0], -1, 4))
+        # detach the bbox because faster rcnn do not backprop through the proposals
+        bbox_preds = self.bbox_coder.decode(anchors, bbox_regress.detach())
+        # Note: bbox_preds is now xyxy formatted as it was converted in .clip_and_nms_output_bboxes
+        # for nms
+        bbox_preds = self.filter_output_bboxes(objness, bbox_preds, image_size)
         if self.training:
-            assert targets is not None, "targets is None in RPN forward()"
+            assert targets is not None, "targets is None in RPN forward() in training mode"
             labels, matched_bboxes = self.assign_anchors_to_targets(anchors, targets)
             labels = self.filter_cross_boundary_anchors(labels, anchors, image_size)
-            loss = self._compute_loss(objness, bbox_regress, anchors, labels, matched_bboxes)
-        
-            
-        return objness, bbox_regress, anchors
+            losses = self._compute_loss(objness, bbox_regress, anchors, labels, matched_bboxes)
+            return bbox_preds, losses
+        # bbox_preds in [xyxy]
+        return bbox_preds
 
 
 
